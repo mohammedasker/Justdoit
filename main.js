3f424d721588379e2ab1bed8695d937269e1f29f
Mohammed Asker <zetoanime@gmail.com>
2019-05-31 11:42:08 +0300

    fixed JS & CSS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var i;
 for (i = 0; i < myNodeList.length; i++) {
 	var span = document.createElement("SPAN");
 	var txt = document.createTextNode("\u00D7");
-	span.className = "Close";
+	span.className = "close";
 	span.appendChild(txt);
 	myNodeList[i].appendChild(span);
 }
@@ -19,7 +19,7 @@ for (i = 0; i < close.length; i++) {
 	}
 }
 
-// Add checked symbol
+// Add checked symbol next to list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
 	if (ev.target.tagName === 'LI') {
@@ -31,6 +31,7 @@ list.addEventListener('click', function(ev) {
 function newElement() {
 	var li = document.createElement("li");
 	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
 	li.appendChild(t);
 	if (inputValue === '') {
 		alert("You must write something!");
@@ -51,64 +52,4 @@ function newElement() {
 			div.style.display = "none";
 		}
 	}
-}
-
-
-////////////////////////////////////
-
-
-// Create a "close" button and append it to each list item
-/*var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }
